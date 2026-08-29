014e290a9d9a6ff0e9e011ab247ec388b9471020
nomad <310685717+dockrelay@users.noreply.github.com>
2026-08-29 18:37:10 -0500

    Lesson_4 binary counter using 4 leds added

---
+++ Lesson_4/cunter.py
@@ -0,0 +1,14 @@
+from machine import Pin
+from time import sleep
+
+led15=Pin(15,Pin.OUT)
+led14=Pin(14,Pin.OUT)
+led13=Pin(13,Pin.OUT)
+led12=Pin(12,Pin.OUT)
+
+led15.value(1)
+led14.value(1)
+led13.value(1)
+led12.value(1)
+
+
